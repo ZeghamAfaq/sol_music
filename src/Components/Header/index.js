d686c079ec529b0a0faa7a7a5fa3d62370307235
ZeghamAfaq <zegham.afaq786@gmail.com>
2022-07-15 17:39:50 +0500

    trying to built header

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -2,26 +2,37 @@ import React from 'react'
 import './style.css'
 const Header = () => {
   return (
-    <>
-    <div className='navbar'>
-        <ul className='logo'>
-            <li><span className='multiColor'>Sol</span>Music</li>
-        </ul>
-        <ul className='nav'>
-            <li>Home</li>
-            <li>About</li>
-            <li>Pages</li>
-            <li>News</li>
-            <li>Contacts</li>
-        </ul>
-        <ul className='reg'>
-            <li>Help</li>
-            <li>|</li>
-            <li>Login</li>
-            <li>Create an account</li>
-        </ul>
+    <div className='mainheader'>
+        <div className='logo'><span>Sol</span>Music</div>
+        <div className='left-nav'>
+            <ul className='navbar'>
+                <li>Home</li>
+                <li>About</li>
+                <li>Pages</li>
+                <li>News</li>
+                <li>Contact</li>
+            </ul>
+        </div>
+        <div className='right-nav'>
+            <div className='help'>
+                <ul>
+                    <li>Help</li>
+                    <li>|</li>
+                </ul>
+            </div>
+            
+            <div className='reg'>
+                <ul>
+                    <li>login</li>
+                    <li>Create an Account</li>
+                </ul>
+            </div>
+        </div>
+        <div className='menu'>
+            <button className='menu-btn'>Menu</button>
+        </div>
+      
     </div>
-    </>
   )
 }
 

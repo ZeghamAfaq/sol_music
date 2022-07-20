61712efcd1fe7bfd8b105665f3e266c25e83c0b7
ZeghamAfaq <zegham.afaq786@gmail.com>
2022-07-20 12:53:43 +0500

    Working on Artist Component

--- src/Components/Artist/index.js
+++ src/Components/Artist/index.js
@@ -1,10 +1,18 @@
 import React from 'react'
 import './style.css'
+import card1 from '../../Images/card1.png'
+import card2 from '../../Images/card2.png'
+import card3 from '../../Images/card3.png'
+import card4 from '../../Images/card4.png'
+
+
 
 const Artist = () => {
   return (
-    <div className='artist'>
-        <div className='artist-heading'>
+    <div className='artist-component'>
+        <div
+        className='artist'>
+            <div className='artist-heading'>
             <h1>
                 Our Concepts &
                 Artists
@@ -24,12 +32,43 @@ const Artist = () => {
                 Lorem Ipsam dollor Lorem Ipsam dollor
             </p>
         </div>
-        <div className='artist-cards'>
-            <div className='artist-card1'>card1</div>
-            <div className='artist-card2'>card2</div>
-            <div className='artist-card3'>card3</div>
-            <div className='artist-card4'>card4</div>
         </div>
+        
+          <div className='artist-cards'>
+            <div className='artist-card1'>
+                <div>
+                    <img src={card1} alt=' Artist' />
+                </div>
+                <div>
+                    <p>SolMusic</p>
+                </div>
+            </div>
+             <div className='artist-card1'>
+                <div>
+                    <img src={card2} alt=' Artist' />
+                </div>
+                <div>
+                    <p>Live Concerts</p>
+                </div>
+            </div>
+             <div className='artist-card1'>
+                <div>
+                    <img src={card3} alt=' Artist' />
+                </div>
+                <div>
+                    <p>Dj Sets</p>
+                </div>
+            </div>
+             <div className='artist-card4'>
+                <div className='artist-card4-img'>
+                    <img src={card4} alt=' Artist' />
+                </div>
+                <div>
+                    <p>Live Streams</p>
+                </div>
+            </div>
+        </div>
+      
     </div>
   )
 }
